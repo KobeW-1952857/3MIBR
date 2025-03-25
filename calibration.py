aef680ec3699620651fd21059862772a60c130ac
KobeW-1952857 <kobe.werner@student.uhasselt.be>
2025-03-25 12:40:05 +0100

    More refactoring, and writing calibration to file

--- calibration.py
+++ calibration.py
@@ -1,27 +1,7 @@
-# import cv2
-# import open3d
-
-
-# image = cv2.imread("./GrayCodes/chess/00.jpg")
-# image = cv2.resize(image, (0, 0), fx=0.1, fy=0.1)
-# corners = cv2.findChessboardCorners(image, (7, 9))
-# print(corners)
-# cv2.imshow("test", image)
-# cv2.imshow("test", image)
-# cv2.waitKey(10000)
-
-# imageLeft = cv2.imread("./GrayCodes/view0/02.jpg")
-# imageLeft = cv2.resize(imageLeft, (0, 0), fx=0.1, fy=0.1)
-# imageRight = cv2.imread("./GrayCodes/view1/02.jpg")
-# imageRight = cv2.resize(imageRight, (0, 0), fx=0.1, fy=0.1)
-# cv2.imshow("test", image)
-# cv2.waitKey(10000)
-
-from genericpath import isfile
-from ntpath import join
-from os import listdir
 import numpy as np
+import numpy.typing as npt
 import cv2 as cv
+import cv2.typing as cvt
 import glob
 
 
@@ -66,10 +46,12 @@ def intrinsic_calibration(file_names: list, grid_size: tuple[int, int]):
     img = cv.resize(img, (0, 0), fx=0.1, fy=0.1)
 
     # Calibration
-    return cv.calibrateCamera(objpoints, imgpoints, img.shape[::2], None, None)
+    return cv.calibrateCamera(
+        objpoints, imgpoints, img.shape[::2], None, None, criteria=criteria
+    )
 
 
-def undistort(file: str, Kmtx: np.array, dist: np.array):
+def undistort(file: str, Kmtx: cvt.MatLike, dist: cvt.MatLike):
     img = cv.imread(file)
     img = cv.resize(img, (0, 0), fx=0.1, fy=0.1)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -79,11 +61,18 @@ def undistort(file: str, Kmtx: np.array, dist: np.array):
     return cv.undistort(gray, Kmtx, dist, None, newcameramtx)
 
 
+def combine_extrinsic_vecs(rvecs, tvecs):
+    extrinsics = []
+    for rvec, tvec in zip(rvecs, tvecs):
+        extrinsic = np.eye(4)
+        extrinsic[:3, :3] = cv.Rodrigues(rvec)[0]
+        extrinsic[:3, 3] = tvec.flatten()
+        extrinsics.append(extrinsic)
+    return extrinsics
+
+
 if __name__ == "__main__":
     files = glob.glob("./GrayCodes/chess/*.jpg")
-    ret, Kmtx, dist, rvecs, tvecs = intrinsic_calibration(files, (7, 9))
-    np.savez("intrinsic_calibration.npz", Kmtx=Kmtx, dist=dist)
-    np.savez("extrinsic_vectors.npz", rvecs=rvecs, tvecs=tvecs)
-
-    cv.imshow("Undistorted", undistort(files[0], Kmtx, dist))
-    cv.waitKey()
+    rms, Kmtx, dist, rvecs, tvecs = intrinsic_calibration(files, (7, 9))
+    extrinsics = combine_extrinsic_vecs(rvecs, tvecs)
+    np.savez("calibration.npz", Kmtx=Kmtx, dist=dist, extrinsics=extrinsics)
